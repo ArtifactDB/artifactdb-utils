9bfc26045d8bc961b348a9bea2e0d767bce0e3f0
Sebastien Lelong <lelongs@gene.com>
2023-01-27 17:10:04 +0000

    support py3.7 for package version detection

--- src/artifactdb/utils/__init__.py
+++ src/artifactdb/utils/__init__.py
@@ -1,12 +1,15 @@
 import sys
 
-from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
+if sys.version_info >= (3, 8):
+    from importlib.metadata import version, PackageNotFoundError
+else:
+    from importlib_metadata import version, PackageNotFoundError
 
 try:
     # Change here if project is renamed and does not equal the package name
-    dist_name = "artifactdb-utils"   # pylint: disable=invalid-name
+    dist_name = "artifactdb-utils"  # pylint: disable=invalid-name
     __version__ = version(dist_name)
-except PackageNotFoundError:  # pragma: no cover
+except PackageNotFoundError as e:  # pragma: no cover
     __version__ = "unknown"
 finally:
     del version, PackageNotFoundError
